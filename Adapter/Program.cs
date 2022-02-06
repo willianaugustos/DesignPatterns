82abbb0f4499d3f911bff22c627ad8827c85bd10
Willian <willian@bytebr.com.br>
2022-02-06 19:11:25 -0300

    Example for Adapter Pattern

--- Adapter/Program.cs
+++ Adapter/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+namespace ExampleAdapterPattern
+{
+    public class Example
+    {
+        public static void Main()
+        {
+            // See https://aka.ms/new-console-template for more information
+            Console.WriteLine("Using of pattern Adapter");
+
+            var domainPerson = new Person("John", "Doe");
+            var personModel = new PersonModelAdapter(domainPerson);
+
+            Console.WriteLine(personModel.FullName);
+        }
+    }
+
+    public class Person
+    {
+        public string FirstName { get; private set; }  
+        public string LastName {get;private set;}
+
+        public Person(string FirstName, string LastName)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+        }
+    }
+
+    public interface IPersonModel {
+        string FullName {get;}
+    }
+
+    public class PersonModelAdapter : IPersonModel
+    {
+        public string FullName {get; private set;}
+        public PersonModelAdapter(Person person)
+        {
+           this.FullName = $"{person.FirstName} {person.LastName}";
+        }
+    }
+}
+
