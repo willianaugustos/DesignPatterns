8aaf426049459951bbaa286b8bfdbfecc6514e9c
Willian <willian@bytebr.com.br>
2022-02-06 18:08:01 -0300

    Example for Factory Pattern

--- Factory/Program.cs
+++ Factory/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Diagnostics;
+
+
+namespace ExampleFactoryPattern
+{
+
+    static class Example
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, lets test the factory pattern:");
+
+            var objEmail = NotificationFactory.Create(NotificationType.Email);
+            objEmail.Notify();
+
+            var objSMS = NotificationFactory.Create(NotificationType.SMS);
+            objSMS.Notify();
+            
+        }
+    }
+
+    enum NotificationType
+    {
+        Email, SMS
+    }
+
+    class NotificationFactory
+    {
+        public static INotifyer Create(NotificationType type)
+        {
+            switch (type)
+            {
+                case NotificationType.Email: return new NotifyByEmail();
+                case NotificationType.SMS: return new NotifyBySMS();
+                default: throw new Exception("Indefined type of INotifyer");
+            }
+        }
+    }
+
+    interface INotifyer
+    {
+        void Notify();
+    }
+
+    class NotifyByEmail : INotifyer
+    {
+        public void Notify()
+        {
+            Console.WriteLine("Notifyed by e-Mail");
+        }
+    }
+
+    class NotifyBySMS : INotifyer
+    {
+        public void Notify()
+        {
+            Console.WriteLine("Notifyed by SMS");
+        }
+    }
+
+}
--- Iterator/Program.cs
+++ Iterator/Program.cs
@@ -1,2 +1,71 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Collections;
+
+namespace Iterator
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            Console.WriteLine("Testing iterator");
+
+            var collection = new CategoryCollection();
+            foreach (var c in collection)
+            {
+                Console.WriteLine(c.Name);
+            }
+        }
+    }
+
+    class Category
+    {
+        public string Name { get; set; }
+    }
+
+    class CategoriesEnumerator : IEnumerator<Category>
+    {
+        private int categoryId = 0;
+
+        public Category Current => new Category
+        {
+            //In really, there is no list, but the items were created just in time
+            Name = $"Category {categoryId}"
+        };
+
+        object IEnumerator.Current => Current;
+
+        public void Dispose()
+        {
+
+        }
+
+        public bool MoveNext()
+        {
+            if (this.categoryId > 5)
+                return false;
+            else {
+                this.categoryId++;
+                return true;
+            }
+        }
+
+        public void Reset()
+        {
+            this.categoryId=0;
+        }
+    }
+
+    class CategoryCollection : IEnumerable<Category>
+    {
+        public IEnumerator<Category> GetEnumerator()
+        {
+            return new CategoriesEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
